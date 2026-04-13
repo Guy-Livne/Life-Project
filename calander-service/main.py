148f3a38fba192734cc52756019420e2fd9e09e4
Guy Livne'git config --global user.email guylivne123@gmail.comgit config --global user.name "Guy Livne <guylivne123@gmail.com>
2026-04-13 20:24:35 +0300

    Containrized The Calender service and the UI service

--- calander-service/main.py
+++ calander-service/main.py
@@ -63,7 +63,7 @@ def create_event(event: dict = Body(...)):
     return {"error": str(error)}
 
 def main():
-  uvicorn.run(app, host="127.0.0.1", port=8000)
+  uvicorn.run(app, host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
   main()
