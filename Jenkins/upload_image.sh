87c212b34435e153885e9d93a87e689cbbf83131
Guy Livne'git config --global user.email guylivne123@gmail.comgit config --global user.name "Guy Livne <guylivne123@gmail.com>
2026-04-16 17:34:22 +0300

    still checking git diff

--- Jenkins/upload_image.sh
+++ Jenkins/upload_image.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
-BASE_DIRECTORY='/home/guy-livne/life-project'
-printf "%s\n" $BASE_DIRECTORY
+BASE_DIRECTORY='/home/guy-livne/life-project/'
+CHANGED_SERVICES=$(git diff --name-only HEAD~1 HEAD | grep -Eo '^[^\/]+')
+
+#Exports needed Environment variables for the build process
+set -a; export "${BASE_DIRECTORY}.env"; set +a
+
+# Run the build command for each changed service
+for SERVICE in $CHANGED_SERVICES; do
+    FULL_SERVICE_PATH="${BASE_DIRECTORY}${SERVICE}"
+    docker build -t "${SERVICE}:latest" "${FULL_SERVICE_PATH}"
+  done  
