5d94a9cbdcb601b770a2d4f058f83b7a536678ee
Guy Livne'git config --global user.email guylivne123@gmail.comgit config --global user.name "Guy Livne <guylivne123@gmail.com>
2026-04-18 00:44:21 +0300

    changes to upload image script

--- Jenkins/upload_image.sh
+++ Jenkins/upload_image.sh
@@ -3,7 +3,7 @@ echo "Starting upload_image.sh script..."
 BASE_DIRECTORY='/home/guy-livne/life-project/'
 CHANGED_SERVICES=$(git diff --name-only HEAD~1 HEAD | grep -Eo '^[^\/]+' | uniq)
 #Exports needed Environment variables for the build process
-set -a; source "${BASE_DIRECTORY}.env"; set +a
+set -a; source ./.env ; set +a
 
 # Run the build command for each changed service
 for SERVICE in $CHANGED_SERVICES; do
