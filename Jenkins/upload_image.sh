d2b40d17fbf6981c460d8be4a04fdcd50e71b526
Guy Livne'git config --global user.email guylivne123@gmail.comgit config --global user.name "Guy Livne <guylivne123@gmail.com>
2026-04-18 00:46:15 +0300

    please work jenkinsgit add .

--- Jenkins/upload_image.sh
+++ Jenkins/upload_image.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 echo "Starting upload_image.sh script..."
-BASE_DIRECTORY='/home/guy-livne/life-project/'
 CHANGED_SERVICES=$(git diff --name-only HEAD~1 HEAD | grep -Eo '^[^\/]+' | uniq)
 #Exports needed Environment variables for the build process
 set -a; source ./.env ; set +a
 
 # Run the build command for each changed service
 for SERVICE in $CHANGED_SERVICES; do
-    FULL_SERVICE_PATH="${BASE_DIRECTORY}${SERVICE}"
-    if [[ $(ls ${FULL_SERVICE_PATH} | grep -c "Dockerfile") -gt 0 ]]; then
+    if [[ $(ls "./${SERVICE}" | grep -c "Dockerfile") -gt 0 ]]; then
         printf "Building and pushing image for service: %s\n" "$SERVICE"
         # Build the Docker image
         docker build -t "${DOCKER_REGISTRY}:${SERVICE}-$(date '+%d.%m.%Y-%k:%M:%S')" "./${SERVICE}"
