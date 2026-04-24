95bf88ef53e9e4ebf81cc69119f803ca7dde405a
Guy Livne'git config --global user.email guylivne123@gmail.comgit config --global user.name "Guy Livne <guylivne123@gmail.com>
2026-04-24 18:29:51 +0300

    updated the upload_image script

--- Jenkins/upload_image.sh
+++ Jenkins/upload_image.sh
@@ -1,16 +1,24 @@
 #! /bin/bash
 echo "Starting upload_image.sh script..."
-CHANGED_SERVICES=$(git diff --name-only HEAD~1 HEAD | grep -Eo '^[^\/]+' | uniq)
+CURRENT_COMMIT_TAG=$(git tag --points-at HEAD)
+VERSION_REGEX='v[0-9]+\.[0-9]+\.[0-9]+'
+SERVICE_REGEX='^[a-zA-Z0-9_-]+'  
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+  echo "Loaded environment variables from .env"
+fi
 #Environment variables are set by Jenkins (DOCKER_REGISTRY, DOCKER_REPO_PAT)
+SERVICE=$(echo "$CURRENT_COMMIT_TAG" | grep -oE "$SERVICE_REGEX")
+VERSION=$(echo "$CURRENT_COMMIT_TAG" | grep -oE "$VERSION_REGEX")
+echo "Current Version: $VERSION"
+echo "Current Service: $SERVICE"
 
 # Run the build command for each changed service
-for SERVICE in $CHANGED_SERVICES; do
-    if [[ $(ls "./${SERVICE}" | grep -c "Dockerfile") -gt 0 ]]; then
-        printf "Building and pushing image for service: %s\n" "$SERVICE"
-        # Build the Docker image
-        docker build -t "${DOCKER_REGISTRY}:${SERVICE}-$(date '+%d.%m.%Y-%k:%M:%S')" "./${SERVICE}"
-        # Push the Docker image
-        docker login -u "guylivne1" -p "${DOCKER_REPO_PAT}"
-        docker push "${DOCKER_REGISTRY}:${SERVICE}"
-    fi
-done
+if [[ $(ls "./${SERVICE}" | grep -c "Dockerfile") -gt 0 ]]; then
+    printf "Building and pushing image for service: %s\n" "$SERVICE"
+    # Build the Docker image
+    docker build -t "${DOCKER_REGISTRY}${SERVICE}:${VERSION}" "./${SERVICE}"
+    # Push the Docker image
+    echo $GITLAB_REGISTRY_PAT | docker login -u "$GITLAB_USERNAME" --password-stdin "$DOCKER_REGISTRY"
+    docker push "${DOCKER_REGISTRY}${SERVICE}:${VERSION}"
+fi
